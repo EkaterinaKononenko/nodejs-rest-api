d80db491747ae865f809688fcdfa283359ddc68e
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-08 23:10:49 +0200

    add user login

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,8 @@
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { User } = require("../models/user");
 const { ctrlWrapper, HttpError } = require("../helpers");
+const { SECRET_KEY } = process.env;
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -19,6 +21,29 @@ const signup = async (req, res) => {
     });
 }
 
+const login = async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(401, "Email or password invalid");
+  }
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalid");
+  }
+
+  const payload = {
+    id: user._id,
+  };
+
+  const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
+
+  res.json({
+    token,
+  });
+}
+
 module.exports = {
-    signup: ctrlWrapper(signup),
+  signup: ctrlWrapper(signup),
+  login: ctrlWrapper(login),
 }
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -7,4 +7,6 @@ const router = express.Router();
 
 router.post("/signup", validateBody(schemas.signupSchema), ctrl.signup);
 
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
+
 module.exports = router;
