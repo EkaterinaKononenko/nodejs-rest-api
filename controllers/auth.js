40cd62c13d8a0cbeabc1363ba8d7d7870aff0072
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-08 22:14:02 +0200

    add unique email and hash password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,17 @@
+const bcrypt = require("bcryptjs");
 const { User } = require("../models/user");
-const { ctrlWrapper } = require("../helpers");
+const { ctrlWrapper, HttpError } = require("../helpers");
 
 const signup = async (req, res) => {
-    const newUser = await User.create(req.body);
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+
+  if (user) {
+    throw HttpError(409, "Email already exists");
+  }
+  
+  const hashPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({...req.body, password: hashPassword});
     res.status(201).json({
       email: newUser.email,
       name: newUser.name,
