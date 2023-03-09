fd913e4882c8690204190101081f2318aa21c2eb
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-09 14:28:51 +0200

    add logout

--- models/user.js
+++ models/user.js
@@ -25,11 +25,11 @@ const userSchema = new Schema(
       type: String,
       enum: ["starter", "pro", "business"],
       default: "starter",
-    },
+    }, */
     token: {
       type: String,
       default: null,
-    }, */
+    }, 
   }, 
   { versionKey: false, timestamps: true }
 );
