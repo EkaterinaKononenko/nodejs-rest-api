5fb6c9d4e36f27880ba8e7406feb89d822961c6b
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-10 13:30:45 +0200

    update messages

--- helpers/HttpError.js
+++ helpers/HttpError.js
@@ -1,10 +1,10 @@
 const errorMessageList = {
-    400: "Bad Request",
-    401: "Unautorized",
-    403: "Forbidden",
-    404: "Not Found",
-    409: "Conflict"
-}
+  400: "Bad Request",
+  401: "Not authorized",
+  403: "Forbidden",
+  404: "Not Found",
+  409: "Conflict",
+};
 const HttpError = (status, message = errorMessageList[status]) => {
   const error = new Error(message);
   error.status = status;
