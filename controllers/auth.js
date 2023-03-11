ec876c50e4e316f3dd280386b0238b2a0ffa2368
EkaterinaKononenko <kaktysik4@ukr.net>
2023-03-11 22:33:45 +0200

    add update avatar

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,6 +4,9 @@ const { User } = require("../models/user");
 const { ctrlWrapper, HttpError } = require("../helpers");
 const { SECRET_KEY } = process.env;
 const gravatar = require("gravatar");
+const path = require("path");
+const fs = require("fs/promises");
+const avatarsDir = path.join(__dirname, "../", "public", "avatars");
 
 const signup = async (req, res) => {
   const { email, password } = req.body;
@@ -57,10 +60,23 @@ const logout = async (req, res) => {
   res.json({message: "Logout success"})
 }
 
+const updateAvatar = async (req, res) => {
+  const { _id } = req.user;
+  const { path: tempUpload, originalname } = req.file;
+  const filename = `${_id}_${originalname}`
+  const resultUpload = path.join(avatarsDir, filename);
+  await fs.rename(tempUpload, resultUpload);
+  const avatarURL = path.join("avatars", filename);
+  await User.findByIdAndUpdate(_id, { avatarURL });
+
+  res.json({ avatarURL,})
+}
+
 
 module.exports = {
   signup: ctrlWrapper(signup),
   login: ctrlWrapper(login),
   getCurrent: ctrlWrapper(getCurrent),
   logout: ctrlWrapper(logout),
+  updateAvatar: ctrlWrapper(updateAvatar),
 }
